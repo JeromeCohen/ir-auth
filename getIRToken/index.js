2653cb7e469ec23d62d9afd6dd16a74f0e5c9aed
jeromecohen <jeromecohen98@gmail.com>
2019-06-06 19:36:09 -0400

    prod ready

---
+++ getIRToken/index.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+module.exports = async function(context,req) {
+    headers = {
+        'Ocp-Apim-Subscription-Key': process.env['SUBSCRIPTION_KEY'],
+        'content-type': 'application/x-www-form-urlencoded'
+    }
+    await axios.post(process.env['ENDPOINT'] + '/issueToken', {}, {headers: headers}).then((res) => {
+      context.res = {
+        status: 200,
+        body: res.data
+      }
+    }).catch((err) => {
+      context.res = {
+        status: 500,
+        body: 'error'
+      }
+    })
+};

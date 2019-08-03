aa1e110b090b1f5c911702168900dc46ee4f05ae
jeromecohen <jeromecohen98@gmail.com>
2019-08-03 16:52:12 -0700

    Updated for new auth

--- getIRToken/index.js
+++ getIRToken/index.js
@@ -1,20 +1,27 @@
 const axios = require('axios');
+const querystring = require('querystring');
 
-module.exports = async function(context,req) {
-    headers = {
-        'Ocp-Apim-Subscription-Key': process.env['SUBSCRIPTION_KEY'],
-        'content-type': 'application/x-www-form-urlencoded'
-    }
-    await axios.post(process.env['ENDPOINT'] + '/issueToken', {}, {headers: headers}).then((res) => {
-      context.res = {
-        status: 200,
-        headers: {'Access-Control-Allow-Origin' : '*'},
-        body: res.data
-      }
-    }).catch((err) => {
-      context.res = {
-        status: 500,
-        body: 'error'
-      }
-    })
+module.exports = async function (context, req) {
+        headers = {
+                'content-type': 'application/x-www-form-urlencoded'
+        }
+
+        const requestBody = {
+                grant_type: 'client_credentials',
+                client_id: process.env['CLIENT_ID'],
+                client_secret: process.env['CLIENT_SECRET'],
+                resource: 'https://cognitiveservices.azure.com/'
+        }
+        await axios.post(`https://login.windows.net/${process.env['TENANT_ID']}/oauth2/token`, querystring.stringify(requestBody), { headers: headers }).then((res) => {
+                context.res = {
+                        status: 200,
+                        headers: { 'Access-Control-Allow-Origin': '*' },
+                        body: res.data
+                }
+        }).catch((err) => {
+                context.res = {
+                        status: 500,
+                        body: err
+                }
+        })
 };
